8101dc498e70d97d14cc94788c0942eb641e4a56
Abd. Rauf Jauhari Izza Mazzidan <raufmazzidan@gmail.com>
2024-08-17 14:48:27 +0700

    feat: add portofolios

--- src/containers/homepage/elements/portofolio/portofolio.tsx
+++ src/containers/homepage/elements/portofolio/portofolio.tsx
@@ -1,21 +1,22 @@
 import Tags from "@/components/tags";
 import Image from "next/image";
 import { PORTOFOLIO_DATA } from "./portofolio.utils";
+import Link from "next/link";
 
 const Portofolio = () => {
   return (
     <section className="my-40">
-      <div className="max-w-4xl m-auto flex items-center justify-center gap-8 px-8">
+      <div className="max-w-4xl m-auto flex flex-col items-center justify-center gap-20 px-8">
         {PORTOFOLIO_DATA.map((porto, i) => (
-          <a href={porto.url} key={i}>
+          <Link href={porto.url} key={i} target="_blank">
             <div className="bg-[#eceff1] hover:bg-[#f6f6f6] hover:scale-[1.02] hover:shadow-portofolio transition ease-out duration-200 rounded-3xl w-full min-h-96 grid grid-cols-2 overflow-hidden">
-              <div className="lg:p-16 p-12 lg:!pr-0 lg:col-span-1 col-span-2">
+              <div className="lg:p-16 p-8 lg:!pr-0 lg:col-span-1 col-span-2">
                 <Image
                   src={porto.logo.src}
                   alt={porto.logo.alt}
                   height={24}
                   width={105}
-                  className="h-6 w-auto"
+                  className="!h-6 w-auto max-w-[105px]"
                 />
 
                 <h2 className="text-3xl font-medium mt-5">{porto.title}</h2>
@@ -39,7 +40,7 @@ const Portofolio = () => {
                 />
               </div>
             </div>
-          </a>
+          </Link>
         ))}
       </div>
     </section>
--- src/containers/homepage/elements/portofolio/portofolio.utils.ts
+++ src/containers/homepage/elements/portofolio/portofolio.utils.ts
@@ -23,4 +23,87 @@ export const PORTOFOLIO_DATA = [
       alt: "Portofolio MyCarrier Internal",
     },
   },
+  {
+    url: "https://mycarrier.telkom.co.id",
+    title: "MyCarrier Customer",
+    stack: [
+      "TypeScript",
+      "Next Js",
+      "Tailwind CSS",
+      "React Query",
+      "Axios",
+      "Unit Testing",
+      "Server Side Rendering",
+    ],
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    logo: {
+      src: "/assets/logo-mycarrier.svg",
+      alt: "Logo MyCarrier Internal",
+    },
+    preview: {
+      src: "/assets/porto-mycarrier-internal.webp",
+      alt: "Portofolio MyCarrier Internal",
+    },
+  },
+  {
+    url: "https://ppob-griyabayar.com",
+    title: "PPOB Griya Bayar",
+    stack: [
+      "TypeScript",
+      "Next Js",
+      "Tailwind CSS",
+      "Server Side Rendering",
+      "SEO",
+      "Wordpress API",
+    ],
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    logo: {
+      src: "/assets/logo-griya-bayar.png",
+      alt: "Logo Griya Bayar",
+    },
+    preview: {
+      src: "/assets/porto-mycarrier-internal.webp",
+      alt: "Portofolio MyCarrier Internal",
+    },
+  },
+  {
+    url: "https://mycarrier.telkom.co.id",
+    title: "MyCarrier Customer",
+    stack: [
+      "TypeScript",
+      "Next Js",
+      "Tailwind CSS",
+      "React Query",
+      "Axios",
+      "Unit Testing",
+      "Server Side Rendering",
+    ],
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    logo: {
+      src: "/assets/logo-mycarrier.svg",
+      alt: "Logo MyCarrier Internal",
+    },
+    preview: {
+      src: "/assets/porto-mycarrier-internal.webp",
+      alt: "Portofolio MyCarrier Internal",
+    },
+  },
+  {
+    url: "https://ppob-griyabayar.com",
+    title: "Hiraeth Parfume",
+    stack: ["TypeScript", "Next Js", "Tailwind CSS", "GSAP Animation"],
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    logo: {
+      src: "/assets/logo-hiraeth.svg",
+      alt: "Logo Hiraeth",
+    },
+    preview: {
+      src: "/assets/porto-mycarrier-internal.webp",
+      alt: "Portofolio MyCarrier Internal",
+    },
+  },
 ];
